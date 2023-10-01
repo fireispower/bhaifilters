53cc5ba676ba87401785c0178a29cb9c005183c5
Starlord <65814962+fireispower@users.noreply.github.com>
2023-10-01 19:41:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("How to Deploy?", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/fyiaru"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/enigma_mainchat")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE Ask", url="https://t.me/firespower")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
